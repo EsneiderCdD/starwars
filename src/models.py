c66a35d937c3ff37f55b161692eae7fdbabe99ca
Esneider Cadavid <114370356+EsneiderCdD@users.noreply.github.com>
2025-04-23 15:55:04 +0000

    Solucion

--- src/models.py
+++ src/models.py
@@ -1,19 +1,86 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Boolean
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
+from datetime import datetime, timezone
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(40), nullable=False, unique=True)
+    password = Column(String(40), nullable=False)
+    full_name = Column(String(200), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    is_active = Column(Boolean, default=True, nullable=False)
+    created = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    
+    favorites = relationship('Favorite', backref='user', lazy=True)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    diameter = Column(String(100), nullable=False)
+    rotation_period = Column(String(100), nullable=False)
+    orbital_period = Column(String(100), nullable=False)
+    gravity = Column(String(100), nullable=False)
+    population = Column(String(100), nullable=False)
+    climate = Column(String(100), nullable=False)
+    terrain = Column(String(100), nullable=False)
+    surface_water = Column(String(100), nullable=False)
+    created = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    
+    favorites = relationship('Favorite', backref='planet', lazy=True)
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    model = Column(String(100), nullable=False)
+    vehicle_class = Column(String(100), nullable=False)
+    manufacturer = Column(String(100), nullable=False)
+    length = Column(String(100), nullable=False)
+    cost_in_credits = Column(String(100), nullable=False)
+    crew = Column(String(100), nullable=False)
+    max_atmosphering_speed = Column(String(100), nullable=False)
+    cargo_capacity = Column(String(100), nullable=False)
+    consumables = Column(String(100), nullable=False)
+    created = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    
+    favorites = relationship('Favorite', backref='vehicle', lazy=True)
+
+class People(Base):
+    __tablename__ = 'people'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    birth_year = Column(String(100), nullable=False)
+    eye_color = Column(String(100), nullable=False)
+    gender = Column(String(100), nullable=False)
+    hair_color = Column(String(100), nullable=False)
+    height = Column(String(20), nullable=False)
+    mass = Column(String(40), nullable=False)
+    skin_color = Column(String(20), nullable=False)
+    homeworld = Column(String(40), nullable=False)
+    created = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    
+    favorites = relationship('Favorite', backref='people', lazy=True)
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    people_id = Column(Integer, ForeignKey('people.id'), nullable=True)
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+render_er(Base, 'diagram.png')
